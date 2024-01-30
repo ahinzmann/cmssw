fcb48e405e3e30f2be0ca0ce9d97486b5126116b
hinzmann <hinzmann@cern.ch>
2024-01-30 10:20:07 +0100

    fix units

--- testbeam_sim_plots.py
+++ testbeam_sim_plots.py
@@ -11,11 +11,17 @@ if __name__=="__main__":
 
  pointsX=array.array("f",[])
  pointsY=array.array("f",[])
-    
  
- for electronEnergy in [2,3,4,5,7,8,9,10]:
-
-  version=str(electronEnergy)+"GeVelectron"
+ def layer(id):
+   kHGCalLayerOffset = 17
+   kHGCalLayerMask = 0x1F
+   return (int(id) >> kHGCalLayerOffset) & kHGCalLayerMask
+ 
+ #for particleEnergy in [2,3,4,5,7,8,9,10]:
+ for particleEnergy in [5]:
+  
+  particle="Muon"
+  version=str(particleEnergy)+"GeV"+particle
 
   print("start ROOT")
   gROOT.Reset()
@@ -46,30 +52,40 @@ if __name__=="__main__":
   #iPos = 11;
     
   histograms=[
-  #("n_SimHits",0,1000,100,"PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO","size","Number of SimHits","Events"),
-  #("SimHit_energy",0,0.01,100,"PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO","energy","SimHit Energy [GeV]","Hits per event"),
-  #("n_Digis",0,100,100,"nanoaodFlatTable_hgcDigiHEbackTable__GENSIMDIGIRECO","size","Number of Digis","Events"),
-  #("Digi_mips",0,100,1000,"nanoaodFlatTable_hgcDigiHEbackTable__GENSIMDIGIRECO","data","Number of MIPs","Hits per event"),
-  #("n_UncalibratedRecHits",0,100,100,"HGCUncalibratedRecHitsSorted_HGCalUncalibRecHit_HGCHEBUncalibRecHits_GENSIMDIGIRECO","size","Number of uncalibrated RecHits","Events"),
-  #("UncalibratedRecHits_adc",0,100,100,"HGCUncalibratedRecHitsSorted_HGCalUncalibRecHit_HGCHEBUncalibRecHits_GENSIMDIGIRECO","amplitude","ADC count","Hits per event"),
-  #("n_RecHits",0,100,100,"HGCRecHitsSorted_HGCalRecHit_HGCHEBRecHits_GENSIMDIGIRECO","size","Number of calibrated RecHits","Events"),
-  #("RecHits_energy",0,5,100,"HGCRecHitsSorted_HGCalRecHit_HGCHEBRecHits_GENSIMDIGIRECO","energy","Calibrated RecHit Energy [GeV]","Hits per event"),
-  #("n_LayerClusters",0,100,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","size","Number of LayerClusters","Events"),
-  #("LayerCluster_energy",0,5,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","energy","LayerCluster Energy [GeV]","Clusters per event"),
-  #("LayerCluster_x",-100,100,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","x","LayerCluster x [cm]","Clusters per event"),
-  #("LayerCluster_y",0,200,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","y","LayerCluster y [cm]","Clusters per event"),
-  #("LayerCluster_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","z","LayerCluster z [cm]","Clusters per event"),
+  ("n_SimHits",0,1000,100,"PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO","size","Number of SimHits","Events"),
+  ("SimHit_n_vs_layer",0,35,36,"PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO","n","Layer number","SimHits per layer"),
+  ("SimHit_energy",0,0.01,100,"PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO","energy","SimHit energy [GeV]","Hits per event"),
+  ("SimHit_energy_vs_layer",0,35,36,"PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO","energy","Layer number","SimHit energy per layer [GeV]"),
+  #("n_DigisNano",0,100,100,"nanoaodFlatTable_hgcDigiHEbackTable__GENSIMDIGIRECO","size","Number of Digis","Events"),
+  #("DigiNano_data",0,500,100,"nanoaodFlatTable_hgcDigiHEbackTable__GENSIMDIGIRECO","data","ADC counts","Hits per event"),
+  ("n_Digis",0,100,100,"DetIdHGCSampleHGCDataFramesSorted_mix_HGCDigisHEback_GENSIMDIGIRECO","size","Number of Digis","Events"),
+  ("Digi_n_vs_layer",0,35,36,"DetIdHGCSampleHGCDataFramesSorted_mix_HGCDigisHEback_GENSIMDIGIRECO","n","Layer number","Number of Digis per layer"),
+  ("Digi_data",0,500,100,"DetIdHGCSampleHGCDataFramesSorted_mix_HGCDigisHEback_GENSIMDIGIRECO","data","ADC counts","Hits per event"),
+  ("Digi_data_vs_layer",0,35,36,"DetIdHGCSampleHGCDataFramesSorted_mix_HGCDigisHEback_GENSIMDIGIRECO","data","Layer number","ADC counts per layer"),
+  ("n_UncalibratedRecHits",0,100,100,"HGCUncalibratedRecHitsSorted_HGCalUncalibRecHit_HGCHEBUncalibRecHits_GENSIMDIGIRECO","size","Number of uncalibrated RecHits","Events"),
+  ("UncalibratedRecHits_n_vs_layer",0,35,36,"HGCUncalibratedRecHitsSorted_HGCalUncalibRecHit_HGCHEBUncalibRecHits_GENSIMDIGIRECO","n","Layer number","Number of RecHits per layer"),
+  ("UncalibratedRecHits_amplitude",0,100,100,"HGCUncalibratedRecHitsSorted_HGCalUncalibRecHit_HGCHEBUncalibRecHits_GENSIMDIGIRECO","amplitude","Number of MIPs","Hits per event"),
+  ("UncalibratedRecHits_amplitude_vs_layer",0,35,36,"HGCUncalibratedRecHitsSorted_HGCalUncalibRecHit_HGCHEBUncalibRecHits_GENSIMDIGIRECO","amplitude","Layer number","Number of MIPs per layer"),
+  ("n_RecHits",0,100,100,"HGCRecHitsSorted_HGCalRecHit_HGCHEBRecHits_GENSIMDIGIRECO","size","Number of calibrated RecHits","Events"),
+  ("RecHits_n_vs_layer",0,35,36,"HGCRecHitsSorted_HGCalRecHit_HGCHEBRecHits_GENSIMDIGIRECO","n","Layer number","Number of RecHits per layer"),
+  ("RecHits_energy",0,5,100,"HGCRecHitsSorted_HGCalRecHit_HGCHEBRecHits_GENSIMDIGIRECO","energy","Calibrated RecHit Energy [GeV]","Hits per event"),
+  ("RecHits_energy_vs_layer",0,35,36,"HGCRecHitsSorted_HGCalRecHit_HGCHEBRecHits_GENSIMDIGIRECO","energy","Layer number","Energy sum per layer [GeV]"),
+  ("n_LayerClusters",0,100,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","size","Number of LayerClusters","Events"),
+  ("LayerCluster_energy",0,5,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","energy","LayerCluster Energy [GeV]","Clusters per event"),
+  ("LayerCluster_x",-100,100,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","x","LayerCluster x [cm]","Clusters per event"),
+  ("LayerCluster_y",0,200,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","y","LayerCluster y [cm]","Clusters per event"),
+  ("LayerCluster_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","z","LayerCluster z [cm]","Clusters per event"),
   ("LayerCluster_energy_vs_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","energy","LayerCluster z [cm]","Energy sum per event [GeV]"),
-  #("LayerCluster_x_vs_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","x","LayerCluster z [cm]","Average |x| per cluster"),
-  #("LayerCluster_y_vs_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","y","LayerCluster z [cm]","Average |y| per cluster"),
-  #("n_MultiClusters",0,10,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","size","Number of MultiClusters","Events"),
-  #("MultiCluster_energy",0,5,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","energy","MultiCluster Energy [GeV]","Clusters per event"),
-  #("MultiCluster_x",-100,100,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","x","MultiCluster x [cm]","Cluster per events"),
-  #("MultiCluster_y",0,200,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","y","MultiCluster y [cm]","Clusters per event"),
-  #("MultiCluster_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","z","MultiCluster z [cm]","Clusters per event"),
-  #("MultiCluster_energy_vs_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","energy","MultiCluster z [cm]","Energy sum per event [GeV]"),
-  #("MultiCluster_x_vs_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","x","MultiCluster z [cm]","Average |x| per cluster"),
-  #("MultiCluster_y_vs_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","y","MultiCluster z [cm]","Average |y| per cluster"),
+  ("LayerCluster_x_vs_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","x","LayerCluster z [cm]","Average |x| per cluster"),
+  ("LayerCluster_y_vs_z",400,550,100,"recoCaloClusters_hgcalLayerClustersHSci__GENSIMDIGIRECO","y","LayerCluster z [cm]","Average |y| per cluster"),
+  ("n_MultiClusters",0,10,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","size","Number of MultiClusters","Events"),
+  ("MultiCluster_energy",0,5,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","energy","MultiCluster Energy [GeV]","Clusters per event"),
+  ("MultiCluster_x",-100,100,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","x","MultiCluster x [cm]","Cluster per events"),
+  ("MultiCluster_y",0,200,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","y","MultiCluster y [cm]","Clusters per event"),
+  ("MultiCluster_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","z","MultiCluster z [cm]","Clusters per event"),
+  ("MultiCluster_energy_vs_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","energy","MultiCluster z [cm]","Energy sum per event [GeV]"),
+  ("MultiCluster_x_vs_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","x","MultiCluster z [cm]","Average |x| per cluster"),
+  ("MultiCluster_y_vs_z",400,550,100,"recoHGCalMultiClusters_hgcalMultiClusters__GENSIMDIGIRECO","y","MultiCluster z [cm]","Average |y| per cluster"),
     ]
   hists={}
   
@@ -77,7 +93,7 @@ if __name__=="__main__":
     canvas = TCanvas(name+"_", name+"_", 0, 0, 300, 300)
     hists[name]=TH1F(name,name,nbins,xmin,xmax)
     events=TChain("Events")
-    events.Add("gensimdigireco_"+str(electronEnergy)+".root")
+    events.Add("gensimdigireco_"+particle.lower().replace("electron","")+str(particleEnergy)+".root")
     for event in events:
       prod=getattr(event,branch_name).product()
       if var=="size":
@@ -91,12 +107,27 @@ if __name__=="__main__":
         for p in prod:
          if "_x_vs_z" in name or "_y_vs_z" in name:
            hists[name].Fill(getattr(p,name.split("_")[-1])(),abs(getattr(p,var)()-160.*("_y_vs_z" in name)))
+         elif "vs_layer" in name:
+          if "n_vs_layer" in name:
+           y=1
+          elif "DetIdHGCSampleHGCDataFramesSorted" in branch_name:
+           y=getattr(p,var)()[2].data() # index 2 of 5 time-samples
+          else:
+           y=getattr(p,var)()
+          if "PCaloHits_g4SimHits_HGCHitsHEback_GENSIMDIGIRECO" in branch_name:
+           x=layer(p.id())
+          else:
+           x=layer(p.id().rawId())
+          hists[name].Fill(x,y)
          else: 
            hists[name].Fill(getattr(p,name.split("_")[-1])(),getattr(p,var)())
         if ("_x_vs_z" in name or "_y_vs_z" in name) and len(prod)>0:
           hists[name].Scale(1./len(prod))
       else:
         for p in prod:
+         if "data" in name and "DetIdHGCSampleHGCDataFramesSorted" in branch_name:
+          hists[name].Fill(getattr(p,var)()[2].data()) # index 2 of 5 time-samples
+         else:
           hists[name].Fill(getattr(p,var)())
     if not var=="size":
       hists[name].Scale(1./events.GetEntries())
@@ -108,7 +139,7 @@ if __name__=="__main__":
     hists[name].GetXaxis().SetRangeUser(xmin,xmax)
     #hists[name].GetYaxis().SetRangeUser(0.0005,1)
     
-    l=TLegend(0.6,0.88,0.9,0.9,str(electronEnergy)+" GeV Electron")
+    l=TLegend(0.6,0.88,0.9,0.9,str(particleEnergy)+" GeV "+particle)
     l.SetFillStyle(0)
     l.SetTextSize(0.05)
     l.Draw("same")
@@ -123,7 +154,7 @@ if __name__=="__main__":
     canvas.SaveAs(name+"_"+version+".pdf")
     
     if "energy_vs_z" in name:
-       pointsX.append(electronEnergy)
+       pointsX.append(particleEnergy)
        pointsY.append(hists[name].Integral())
 
  canvas = TCanvas("Energy_reconstruction_linearity", "Energy_reconstruction_linearity", 0, 0, 300, 300)
